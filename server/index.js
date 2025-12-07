4435154ab0997cc3c7c75a651d865898b932a88e
sandadi Sreenu <sreenu.friends18@gmail.com>
2025-12-07 17:49:56 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "*",
+    origin: "https://stonesonline.netlify.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
