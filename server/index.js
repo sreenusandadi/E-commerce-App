a51dc8ffd4bf3ea21f06e077ec5ed66d7f0344a4
sandadi Sreenu <sreenu.friends18@gmail.com>
2025-12-07 17:04:25 +0530

    Updated Index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://e-commerce-app-qdz6.onrender.com/",
+    ],
     credentials: true,
   })
 );
